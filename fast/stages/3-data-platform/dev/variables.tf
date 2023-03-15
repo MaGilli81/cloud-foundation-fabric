47e0d42ea7811e0d96e5271633a613062a974b27
Matt Gilliam <50531124+MaGilli81@users.noreply.github.com>
2023-03-15 15:07:31 -0500

    Update variables.tf

--- fast/stages/3-data-platform/dev/variables.tf
+++ fast/stages/3-data-platform/dev/variables.tf
@@ -1,16 +1,3 @@
-# Copyright 2022 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 # tfdoc:file:description Terraform Variables.
 
@@ -128,7 +115,7 @@ variable "host_project_ids" {
 variable "location" {
   description = "Location used for multi-regional resources."
   type        = string
-  default     = "eu"
+  default     = "us"
 }
 
 variable "network_config_composer" {
@@ -183,7 +170,7 @@ variable "project_services" {
 variable "region" {
   description = "Region used for regional resources."
   type        = string
-  default     = "europe-west1"
+  default     = "us-central1"
 }
 
 variable "service_encryption_keys" {
